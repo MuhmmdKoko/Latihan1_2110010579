7d817e242d09769ffa6d8dd48a2bb16912dac29f
MuhmmdKoko <someanncat@gmail.com>
2023-06-12 17:53:38 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author USER
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika koko = new Matematika (7,9);
+        
+        System.out.println("Hasil penjumlahan: "+koko.setPenjumlahan());
+        System.out.println("Hasil pengurangan: "+koko.setPengurangan());
+        System.out.println("Hasil perkalian: "+koko.setPerkalian());
+        System.out.println("Hasil pembagian: "+koko.setPembagian());
+    }
 }
